0ca3c0e24709dca3884bacb9c35076db33cc80fa
j-jayes <j0nathanjayes@gmail.com>
2021-09-04 20:13:51 +0200

    upload scraper

---
+++ NBER-methods.R
@@ -0,0 +1,77 @@
+#' ---
+#' title: "NBER-methods"
+#' author: "JJayes"
+#' date: "04/09/2021"
+#' output: html_document
+#' ---
+#' 
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+library(tidyverse)
+library(rvest)
+library(nberwp)
+library(glue)
+
+#' 
+#' ## Planning
+#' 
+#' Try out Ben Davies package and see if I can access the text of the abstracts.
+#' 
+#' ## NBER papers package
+#' 
+## -----------------------------------------------------------------------------
+papers <- nberwp::papers
+papers
+
+#' 
+#' Nice! Now we have a list of papers - we can construct the URL based on the paper number
+#' 
+#' ### Try scraping?
+#' 
+## -----------------------------------------------------------------------------
+
+# url <- "https://www.nber.org/papers/w8001"
+# 
+# text <- read_html(url) %>% 
+#     html_node("p") %>% 
+#     html_text() %>% 
+#     str_squish()
+
+#' 
+#' Would actually be really cool to look at the different topics popularity over time, like "Kuznets curve" etc. Other interesting words might be, "case studies" or "paradox"
+#' 
+#' Make a function to scrape the abstract:
+#' 
+## -----------------------------------------------------------------------------
+get_abstract <- function(paper){
+    
+    url <- glue("https://www.nber.org/papers/", paper)
+    
+    text <- read_html(url) %>% 
+    html_node("p") %>% 
+    html_text() %>% 
+    str_squish()
+    
+    text
+}
+
+
+#' 
+#' Use function
+#' 
+## -----------------------------------------------------------------------------
+df <- papers %>% 
+    mutate(row_num = row_number()) %>% 
+    filter(between(row_num, 0, 10)) %>%
+    mutate(abstract = map(paper, possibly(get_abstract, "failed")))
+
+df %>% write_rds("data/abstracts_df.rds")
+
+
+#' 
+#' 
+## -----------------------------------------------------------------------------
+knitr::purl("code/NBER-methods.Rmd", documentation = 2)
+
+#' 
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NBER-methods
 Parse and visualize methods from NBER Abstracts
 
+There is a long tradition of economists being seriously concerned about the methods used to answer their questions, sometimes with even more concern that the answers to their questions. It has been documented since [Richard S. Thorn's *The Methodologies of Economists: A Survey*](https://www.jstor.org/stable/40324748?seq=1#metadata_info_tab_contents).
+
 There is a figure produced by [The Economist](https://www.economist.com/finance-and-economics/2016/11/24/economists-are-prone-to-fads-and-the-latest-is-machine-learning) about the latest fad in the discipline being machine learning.
 
 ![](images/methods.png)
